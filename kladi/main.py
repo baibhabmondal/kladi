bc13cef86236215c6ef59be3532ba3eadd4a3f05
Anirudh <icyph0x@pm.me>
2018-04-28 16:37:55 +0530

    fix: file uploading using POST

--- kladi/main.py
+++ kladi/main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+from flask_uploads import *
 
-UPLOAD_FOLDER = '/upload'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'gif', 'jpeg', 'mp4', 'mkv', 'webm', 'tex'])
 
 app = Flask(__name__, 
             static_folder = '../dist/static', 
             template_folder = '../dist') 
+app.config['UPLOADS_DEFAULT_DEST'] = app.root_path
+files = UploadSet('files', ('txt', 'pdf', 'png', 'jpg', 'gif', 'jpeg', 'mp4', 'mkv', 'webm', 'tex'))
+configure_uploads(app, files)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -25,14 +26,12 @@ def allowed_file(filename):
     return '.' in filename and \
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/api/upload')
+@app.route('/api/upload', methods = ['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         f = request.files['file']
         f.save(secure_filename(f.filename))
         return 'OK'
-    else:
-        return 'FAIL'
 
-
-app.run()
+if __name__ == '__main__':
+    app.run(debug = True)

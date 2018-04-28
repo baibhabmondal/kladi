312c540da254f5f40965ff800b39a3edd7d4727f
Anirudh <icyph0x@pm.me>
2018-04-28 12:57:25 +0530

    feat: prep file upload

--- kladi/main.py
+++ kladi/main.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-
+import os
 from flask import Flask, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/upload'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'gif', 'jpeg', 'mp4', 'mkv', 'webm', 'tex'])
 
 app = Flask(__name__, 
             static_folder = './dist/static', 
             template_folder = './dist') 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+@app.route('/api/upload')
 def index():
     return render_template('index.html')
+
+app.run()
